935b5616e91ecccbd54d2e9298d7e10dff0b210e
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-20 21:25:10 -0500

    Version 1.0.4

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,81 +1,88 @@
-/**
+/*
  * Bailey Thompson
- * Word Count (1.0.3)
- * 6 February 2017
- * Info: Create a .txt file and insert some text. The program will then count the amount of words and write it to a .anl
- * Info: file. Insert words as command-line arguments to get the occurences of those words in the .anl file also.
+ * Word Count (1.0.4)
+ * 20 February 2017
+ * Create a .txt file and insert some text. The program will then count the amount of words and write it to a .anl file.
+ * Insert words as command-line arguments to get the occurrences of those words in the .anl file also.
  */
+
 #include <iostream>
 #include <fstream>
 #include <math.h>
 
+const int MAX_CHARACTERS = 50000;
+
+void setFormatToAnl(char* fileName, int index) {
+    fileName[index + 1] = 'a';
+    fileName[index + 2] = 'n';
+    fileName[index + 3] = 'l';
+    fileName[index + 4] = '\0';
+}
+
+void setFormatToTxt(char* fileName, int index) {
+    fileName[index] = '.';
+    fileName[index + 1] = 't';
+    fileName[index + 2] = 'x';
+    fileName[index + 3] = 't';
+    fileName[index + 4] = '\0';
+}
+
+bool invalidFileFormat(char* fileFormat, int u) {
+    return fileFormat[u] == '.' && (fileFormat[u + 1] != 't' || fileFormat[u + 2] != 'x' || fileFormat[u + 3] != 't');
+}
+
 int main(int argc, char* argv[]) {
-    //used for statistics tracking
-    int wordCount = 0, letterCount = 0, longestWord = 0, shortestWord = 0, currentWord = 0;
-    //character buffer
-    char buffer[50000];
-    for (int i = 0; i < 50000; i++) {
+    char buffer[MAX_CHARACTERS];
+    for (int i = 0; i < MAX_CHARACTERS; i++) {
         buffer[i] = ' ';
     }
-    //error checking for insufficient arguments
     if (argc < 2) {
         std::cerr << "Error: not enough arguments" << std::endl;
         return -1;
     }
     char* fileFormat = argv[1];
     int u = 0;
-    //checking file format
     while (fileFormat[u] != '\0' && fileFormat[u] != '.') {
-        u ++;
+        u++;
     }
-    if (fileFormat[u] == '.') {
-        if (fileFormat[u + 1] != 't' && fileFormat[u] != 'x' && fileFormat[u] != 't') {
-            std::cerr << "Error: invalid file format" << std::endl;
-            return -1;
-        }
+    if (invalidFileFormat(fileFormat, u)) {
+        std::cerr << "Error: invalid file format" << std::endl;
+        return -1;
     }
-    //setting file name
     std::ifstream fin;
     int counter = 0;
     char* newFile = argv[1];
     while (newFile[counter] != '\0' && newFile[counter] != '.') {
-        counter += 1;
+        counter++;
     }
     char fileName[counter + 4];
-    int counter2 = 0;
+    int index = 0;
     if (newFile[counter] == '\0') {
-        while (newFile[counter2] != '\0') {
-            fileName[counter2] = newFile[counter2];
-            counter2 += 1;
+        while (newFile[index] != '\0') {
+            fileName[index] = newFile[index];
+            index += 1;
         }
-        fileName[counter2] = '.';
-        fileName[counter2 + 1] = 't';
-        fileName[counter2 + 2] = 'x';
-        fileName[counter2 + 3] = 't';
-        fileName[counter2 + 4] = '\0';
+        setFormatToTxt(fileName, index);
     } else if (newFile[counter] == '.') {
-        while (newFile[counter2] != '\0') {
-            fileName[counter2] = newFile[counter2];
-            counter2 += 1;
+        while (newFile[index] != '\0') {
+            fileName[index] = newFile[index];
+            index += 1;
         }
-        counter2 -= 4;
-        fileName[counter2 + 4] = '\0';
+        index -= 4;
+        fileName[index + 4] = '\0';
     }
     fin.open(fileName);
-    //error checking for lack of file
     if (fin.fail()) {
         std::cerr << "Error: bad file name" << std::endl;
         return -1;
     }
-    //used for subwords
     int subword[argc - 1];
     for (int i = 0; i < argc - 1; i++) {
         subword[i] = 0;
     }
-    //getting file
-    while (fin.getline(buffer, 50000)) {
-        for (int i = 0; i < 50000; i++) {
-            //checking for subword matches
+    int wordCount = 0, letterCount = 0, longestWord = 0, shortestWord = 0, currentWord = 0;
+    while (fin.getline(buffer, MAX_CHARACTERS)) {
+        for (int i = 0; i < MAX_CHARACTERS; i++) {
             int w = 2;
             while (argc > w) {
                 char* temp = argv[w];
@@ -91,17 +98,15 @@ int main(int argc, char* argv[]) {
                 }
                 w += 1;
             }
-            //converting to character array
-            buffer[i] = tolower(buffer[i]);
+            buffer[i] = (char) tolower(buffer[i]);
             if (int(buffer[i]) < 97 || int(buffer[i]) > 122) {
                 buffer[i] = ' ';
             }
-            //doing the logic
             if (buffer[i] != ' ') {
-                currentWord += 1;
-                letterCount += 1;
+                currentWord++;
+                letterCount++;
             } else if (i > 0 && buffer[i] == ' ' && buffer[i - 1] != ' ') {
-                wordCount += 1;
+                wordCount++;
                 if (shortestWord == 0) {
                     shortestWord = currentWord;
                 }
@@ -113,31 +118,28 @@ int main(int argc, char* argv[]) {
                 currentWord = 0;
             }
         }
-        for (int i = 0; i < 50000; i++) {
+        for (int i = 0; i < MAX_CHARACTERS; i++) {
             buffer[i] = ' ';
         }
     }
     fin.close();
-    //changing file type to anl
-    fileName[counter2 + 1] = 'a';
-    fileName[counter2 + 2] = 'n';
-    fileName[counter2 + 3] = 'l';
-    fileName[counter2 + 4] = '\0';
-    //opening file
+
+    setFormatToAnl(fileName, index);
+
     std::ofstream outputFile;
     outputFile.open(fileName);
-    //doing average
-    double average = double(letterCount) / double(wordCount);
+
+    double average = (double) letterCount / (double) wordCount;
     average *= 100;
     average = round(average);
     average /= 100;
-    //writing to file
+
     outputFile << "Number of words: " << wordCount << std::endl;
     outputFile << "Longest word: " << longestWord << std::endl;
     outputFile << "Shortest word: " << shortestWord << std::endl;
     outputFile << "Average word length: " << average << std::endl;
     for (int i = 2; i < argc; i++) {
-        outputFile << "Number of occurences of " << argv[i] << ": " << subword[i - 2] << std::endl;
+        outputFile << "Number of occurrences of " << argv[i] << ": " << subword[i - 2] << std::endl;
     }
 
     outputFile.close();
